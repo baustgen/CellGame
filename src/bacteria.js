91981677db169e7ded6387cd6fc7300e805e27b7
Braeden Austgen <baustgen2615@gmail.com>
2019-11-27 12:16:32 -0800

    randomize bacteria position, velocity and scale

--- src/bacteria.js
+++ src/bacteria.js
@@ -1,9 +1,22 @@
 class Bacteria {
-    constructor({scale}) {
+    constructor() {
+        let pos = Bacteria.randomPosition()
         this.xPos = pos[0];
         this.yPos = pos[1];
-        this.vel = vel;
-        this.scale = scale;
+        this.vel = this.xPos < 500 ? Math.floor(Math.random() * 5) : Math.floor(Math.random() * -5);
+        this.scale = Math.random() * 10;
+    }
+
+    static randomPosition() {
+        let x;
+        let coin = Math.random();
+        if (coin > 0.5) {
+            x = Math.floor(Math.random() * 20);
+        } else {
+            x = 600 - Math.floor(Math.random() * 20);
+        }
+        const y = Math.random() * 350;
+        return [x, y];
     }
 
     draw(ctx) {
