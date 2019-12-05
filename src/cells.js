da13e8eeb55ed83e9f656006a652f723053dec46
Braeden Austgen <baustgen2615@gmail.com>
2019-12-05 10:57:15 -0800

    add gameView class

--- src/cells.js
+++ src/cells.js
@@ -1,13 +1,19 @@
 import Bacteria from './bacteria'
 import User from './user'
 import Game from './game'
+import GameView from './gameView'
+
 
 document.addEventListener('DOMContentLoaded', () => {
     const canvas = document.getElementById('game-canvas');
     const ctx = canvas.getContext("2d")
+    const testGameView = new GameView(ctx);
 
+
+    testGameView.start()
     window.ctx = ctx;
     window.Bacteria = Bacteria;
     window.User = User;
     window.Game = Game;
+    window.GameView = GameView;
 })
---
+++ src/gameView.js
@@ -0,0 +1,20 @@
+import Game from './game'
+
+
+class GameView {
+    constructor(ctx) {
+        this.game = new Game();
+        this.ctx = ctx;
+
+    }
+
+    start() {
+        setInterval(() => {
+            this.game.moveObjects();
+            this.game.draw(this.ctx);
+        }, 20)
+    }
+
+}
+
+export default GameView;
