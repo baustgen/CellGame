6f75a62050806df25114136dc9c653cc048c912c
Braeden Austgen <baustgen2615@gmail.com>
2019-12-05 13:37:13 -0800

    add #remove and remove bacteria when they move off screen

--- src/game.js
+++ src/game.js
@@ -6,8 +6,7 @@ class Game {
     constructor() {
         this.bacteria = []
         this.user = []
-        this.addBacteria(10);
-
+        this.addBacteria(8);
     }
 
     addBacteria(num = 1) {
@@ -19,6 +18,9 @@ class Game {
 
     draw(ctx) {
         ctx.clearRect(0, 0, 600, 400);
+        if (this.bacteria.length < 8) {
+            this.addBacteria(8 - this.bacteria.length)
+        }
         for (let i = 0; i < this.bacteria.length; i++) {
             const bact = this.bacteria[i];
             bact.draw(ctx);
@@ -29,9 +31,19 @@ class Game {
         for (let i = 0; i < this.bacteria.length; i++) {
             const bact = this.bacteria[i];
             bact.move();
+            if (bact.xPos < -100 || bact.xPos > 600) {
+                this.remove(bact)
+            }
         }
     }
 
+    remove(object) {
+        if (object instanceof Bacteria) {
+            this.bacteria.splice(this.bacteria.indexOf(object), 1);
+        } else if (object instanceof User) {
+            this.user.splice(this.user.indexOf(object), 1);
+        }
+    };
 }
 
 export default Game;
