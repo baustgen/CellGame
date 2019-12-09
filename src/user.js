384739a0e6279f9cc6383435fc67653d4d3fe278
Braeden Austgen <baustgen2615@gmail.com>
2019-12-09 14:16:15 -0800

    dont allow user to leave screen

--- src/user.js
+++ src/user.js
@@ -4,45 +4,55 @@ class User {
         this.yPos = 180;
         this.xVel = 0;
         this.yVel = 0;
-        this.scale = 5.5;
+        this.scale = 2.5;
         this.xDim = (20 * this.scale);
         this.yDim = (8 * this.scale);
+        this.src = './documents/images/bacteria_blue_right.png'
     }
 
     draw(ctx) {
-        let img = new Image(10, 10)
-        if (this.vel > 0) {
-            img.src = './documents/images/bacteria_blue_right.png'
-        } else {
-            img.src = './documents/images/bacteria_blue_left.png'
-        }
+        let img = new Image(10, 10);
+        img.src = this.src;
         ctx.drawImage(img, this.xPos, this.yPos, this.xDim, this.yDim);
     }
 
     move() {
-        this.xPos += this.xVel;
-        this.yPos += this.yVel;
+
+        let newX = this.xPos + this.xVel;
+        let newY = this.yPos + this.yVel;
+        if (newX > 600 - (this.xDim / 2) || newX < 0 - (this.xDim / 2)) {
+            this.yPos = newY;
+            this.xVel = 0;
+        } else if (newY > 400 - (this.yDim / 2) || newY < 0 - (this.yDim / 2)) {
+            this.xPos = newX;
+            this.yVel = 0;
+        } else {
+            this.xPos = newX;
+            this.yPos = newY;
+        }
     }
 
     userMove(direction) {
         switch (direction) {
             case 'left':
-                if (this.xVel > -10) {
+                if (this.xVel > -7) {
                     this.xVel -= 1
                 }
+                this.src = './documents/images/bacteria_blue_left.png'
                 break;
             case 'right':
-                if (this.xVel < 10) {
+                if (this.xVel < 7) {
                     this.xVel += 1
                 }
+                this.src = './documents/images/bacteria_blue_right.png'
                 break;
             case 'up':
-                if (this.yVel > -10) {
+                if (this.yVel > -7) {
                     this.yVel -= 1
                 }
                 break;
             case 'down':
-                if (this.yVel < 10) {
+                if (this.yVel < 7) {
                     this.yVel += 1
                 }
                 break;
